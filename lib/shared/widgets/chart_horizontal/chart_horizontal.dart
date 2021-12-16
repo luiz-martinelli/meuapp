b4c4c95cd2fd3fd485078d3f7786752ae743ef60
Luiz Martinelli <luizmartinelli26@outlook.com>
2021-12-16 17:18:36 -0300

    chart

---
+++ lib/shared/widgets/chart_horizontal/chart_horizontal.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChartHorizontal extends StatelessWidget {
+  const ChartHorizontal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
